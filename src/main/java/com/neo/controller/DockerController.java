b0e2fa54a771bac4a44e3f95e81830a38a59dcb4
jzy99 <jzy99@noreply.gitcode.com>
2024-08-08 14:02:06 +0800

    gitee repo update

--- src/main/java/com/neo/controller/DockerController.java
+++ src/main/java/com/neo/controller/DockerController.java
@@ -8,6 +8,6 @@ public class DockerController {
 	
     @RequestMapping("/")
     public String index() {
-        return "Hello Docker!-dev20240808-1130";
+        return "Hello Docker!-dev20240808-11301";
     }
 }
