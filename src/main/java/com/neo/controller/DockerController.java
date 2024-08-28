283d200f87e6931817f2e4cb857e80634c475089
jzy99 <jzy99@hotmail.com>
2024-08-28 12:31:12 +0000

    update src/main/java/com/neo/controller/DockerController.java.
    Signed-off-by: jzy99 <jzy99@hotmail.com>

--- src/main/java/com/neo/controller/DockerController.java
+++ src/main/java/com/neo/controller/DockerController.java
@@ -8,6 +8,6 @@ public class DockerController {
 	
     @RequestMapping("/")
     public String index() {
-        return "Hello Docker!-dev20240828-2028";
+        return "Hello Docker!-dev20240828-20281";
     }
 }
